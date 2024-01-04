c71cee9171907e2542522d48737b5409fd2007a4
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-04 00:57:32 +0000

    feat: add home app and update templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')), # This will include the urls.py file from the home app
 ]
