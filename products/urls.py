d9915540de2788f2d18db6897c87ee84c7f65c55
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-08 01:08:28 +0000

    feat: add product category filtering in views and main-nav.html

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
+    path('', views.all_products, name='products'), # this is the url for the products page
     path('<product_id>/', views.product_detail, name='product_detail'), # <product_id> is a variable that will be passed to the view as a parameter and may need int before the produc_id to convert it to an integer
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,8 +10,14 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None  # to prevent error if no search term is entered
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories) # __in is a Django field lookup that allows us to check if a given item is in a list
+            categories = Category.objects.filter(name__in=categories) # this is used to display the category name in the search box after the search is performed
+        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,6 +35,7 @@ def all_products(request):
         'products': products,
         # this is used to keep the search term in the search box after the search is performed
         'search_term': query,
+        'current_categories': categories, # this is used to display the category name in the search box after the search is performed
     }
 
     return render(request, 'products/products.html', context)
