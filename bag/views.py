504d80e7ae6a5615af9ff884b74bf034e779199a
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-10 04:45:34 +0000

    feat: add support for item sizes in the shopping bag

--- bag/views.py
+++ bag/views.py
@@ -12,14 +12,32 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # below gets the bag variable from the session, or create a new one in an empty dict if it doesn't exist
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    # Gets the bag variable from the session, or create a new one in an empty dict if it doesn't exist
     bag = request.session.get('bag', {})
-    # If the item is already in the bag, update the quantity
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    # Otherwise, add the item to the bag
+
+    if size:
+        # If the item is already in the bag, update the quantity
+        if item_id in list(bag.keys()):
+            # If the size is already in the bag, update the quantity
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            # Otherwise, add the size to the bag
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        # Otherwise, add the item to the bag
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # If the item is already in the bag, update the quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        # Otherwise, add the item to the bag
+        else:
+            bag[item_id] = quantity
+
     # Overwrite the bag variable in the session with the updated version
     request.session['bag'] = bag
     return redirect(redirect_url)
