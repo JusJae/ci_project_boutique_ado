5f385647b9800e98ccf8cf3e2bf43c7b87bafb4b
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-12 01:04:07 +0000

    feat: add toast messages for notifications

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -10,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -37,6 +41,7 @@ def add_to_bag(request, item_id):
         # Otherwise, add the item to the bag
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     # Overwrite the bag variable in the session with the updated version
     request.session['bag'] = bag
