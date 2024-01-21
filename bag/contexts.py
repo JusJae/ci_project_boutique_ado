e3458e3a239b2ddd568b6ee06dd33f1d2e3b0dbd
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-21 00:06:35 +0000

    refactor: bag_contents function

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def bag_contents(request): # This function is available to all templates
-    
+
+def bag_contents(request):  # This function is available to all templates
+
     bag_items = []
     total = 0
     product_count = 0
@@ -21,27 +22,27 @@ def bag_contents(request): # This function is available to all templates
                 'quantity': item_data,
                 'product': product,
             })
-        else: # If the item has a size
+        else:  # If the item has a size
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({  # This is a list of dictionaries
-                'item_id': item_id,
-                'quantity': quantity,
-                'product': product,
-                'size': size,
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
                 })
-    
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-        
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -50,6 +51,6 @@ def bag_contents(request): # This function is available to all templates
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-    } 
-    
+    }
+
     return context
