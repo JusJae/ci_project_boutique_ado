204f5a7656d7f223c377faa79188da9980775615
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-24 21:35:16 +0000

    feat: add webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -28,12 +29,12 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
-
         # we get the payment intent object from the event data
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag  # we get the bag from the metadata
         save_info = intent.metadata.save_info  # we get the save info from the metadata # noqa
+
         # Get the Charge object from the payment intent
         stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
 
@@ -50,6 +51,24 @@ class StripeWH_Handler:
             if value == "":
                 # if it is, we set the value to none
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None # we set the profile to none for anonymous users
+        username = intent.metadata.username  # we get the username from the metadata # noqa
+        if username != 'AnonymousUser':
+            # if the user is not anonymous, we get the profile
+            profile = UserProfile.objects.get(user__username=username)
+            # we check if the save info was checked
+            if save_info:
+                # if it was, we update the profile information
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1 # noqa
+                profile.default_street_address2 = shipping_details.address.line2 # noqa
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False  # we set the order exists variable to false
         attempt = 1  # we set the attempt variable to 1
@@ -96,12 +115,11 @@ class StripeWH_Handler:
                     # we create the order using the information in the payment
                     # intent
                     full_name=shipping_details.name,
-                    # we set the user to none as the user is not logged in
-                    # when the order is created
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
-                    postcode=shipping_details.address.postcode,
+                    postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
