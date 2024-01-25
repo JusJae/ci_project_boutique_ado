9c3c7df672694042d5b304ae60a2636a94a5f590
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-01-25 18:22:59 +0000

    feat: add product admin - product view, url and template

--- products/views.py
+++ products/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    
+
     products = Product.objects.all()
     query = None  # to prevent error if no search term is entered
     categories = None
@@ -17,25 +18,33 @@ def all_products(request):
 
     if request.GET:
         if 'sort' in request.GET:
-            sortkey = request.GET['sort'] # this is the name of the field we want to sort on
-            sort = sortkey # this is used to keep the sort term in the search box after the search is performed
+            sortkey = request.GET['sort']  # name of the field we want to sort
+            sort = sortkey  # this is used to keep the sort term in the
+            # search box after the search is performed
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name')) # annotate current list of products with a new field called lower_name which is the name field converted to lower case
+                products = products.annotate(lower_name=Lower('name'))
+                # annotate current list of products with a new field called
+                # lower_name which is the name field converted to lower case
             if sortkey == 'category':
-                    sortkey = 'category__name'
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey = f'-{sortkey}' # the - in front of the sortkey reverses the order of the sort
+                    sortkey = f'-{sortkey}'
+                    # the - in front of the sortkey reverses the sort order
 
-            products = products.order_by(sortkey) # sort the products by the sortkey
+            products = products.order_by(sortkey)
+            # sort the products by the sortkey
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories) # __in is a Django field lookup that allows us to check if a given item is in a list
-            categories = Category.objects.filter(name__in=categories) # this is used to display the category name in the search box after the search is performed
-        
+            products = products.filter(category__name__in=categories)
+            # __in is a Django field lookup that allows us to check
+            # if a given item is in a list
+            categories = Category.objects.filter(name__in=categories)
+            # displays the category name in the search box after the search
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -43,19 +52,23 @@ def all_products(request):
                     request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            # Q is a complex query that allows us to use the OR operator (|) to build a query that matches on either name or description
+            # Q is a complex query that allows us to use the OR operator (|)
+            # to build a query that matches on either name or description
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
             # filter the products queryset to match the queries
             products = products.filter(queries)
-            
-    current_sorting = f'{sort}_{direction}' 
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        # this is used to keep the search term in the search box after the search is performed
+        # this is used to keep the search term in the
+        # search box after the search is performed
         'search_term': query,
-        'current_categories': categories, # this is used to display the category name in the search box after the search is performed
+        'current_categories': categories,
+        # this is used to display the category name in the
+        # search box after the search is performed
         'current_sorting': current_sorting,
     }
 
@@ -72,3 +85,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
